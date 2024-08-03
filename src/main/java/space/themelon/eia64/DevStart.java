6a367add057303a48c4ac9601b309dc2eec4bb7c
kumaraswamy <care.xoma@gmail.com>
2024-08-03 12:50:43 +0530

    continue working and debugging math interpreter

--- src/main/java/space/themelon/eia64/DevStart.java
+++ src/main/java/space/themelon/eia64/DevStart.java
@@ -3,7 +3,7 @@ package space.themelon.eia64;
 public class DevStart {
   public static void main(String[] args) {
     Main.main(new String[] {
-            "/home/kumaraswamy/Documents/Eia64/examples/math_interpreterv2.eia"
+            "/home/kumaraswamy/Documents/Eia64/testfiles/stdlibtest/simulate_parse_break.eia"
     });
   }
 }
